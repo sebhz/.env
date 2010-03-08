234abacd3f58818bce421b3ea0b8f56d3d3e868e
sebhz <seb@zyglute.fr>
2010-03-08 23:01:50 +0100

    Added more Ion3 config files

---
+++ ion3/local.lua
@@ -0,0 +1,11 @@
+local function close_empty_frame(ftable)
+    if ftable.mode ~= 'remove' then return end
+    local wmp = ftable.reg
+    if WMPlex.mx_count(wmp) == 0 then
+        ioncore.defer(function() WRegion.rqclose(wmp) end)
+    end
+end
+
+local hk=ioncore.get_hook("frame_managed_changed_hook")
+hk:add(close_empty_frame)
+
