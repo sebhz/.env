78a79e314e6c14ab3073c6905cc7e04186a26a26
sebhz <seb@zyglute.fr>
2020-02-25 14:01:45 +0100

    [notion] Remove old notion3 session directories

--- notion/notion3/default-session--1/saved_queryhist.lua
+++
@@ -1,5 +0,0 @@
--- Ce fichier a été généré par /usr/bin/notion, Ne pas éditer.
-return {
-    [1] = "menu.mainmenu:sessions/redémarrer",
-}
-
--- notion/notion3/default-session--1/saved_wd.lua
+++
@@ -1,4 +0,0 @@
--- Ce fichier a été généré par /usr/bin/notion, Ne pas éditer.
-return {
-}
-
