7e01cdc68a2af8ef275c29bd23604c4f196965ea
sebhz <seb@zyglute.fr>
2020-06-06 09:42:29 +0200

    [dwm] Major (!) improvements to dwmstatus

--- dwm/dwmstatus.sh
+++ dwm/dwmstatus.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
+# Caps and num lock (my keyboard has no LED)
 get_mods() {
-    MODS="CN: "$(xset q | grep Caps | tr -s ' ' | cut -d ' ' -f 5,9 | sed 's/on/▣/g' | sed 's/off/▢/g')
+    MON_MODS=$(xset q | grep Caps | tr -s ' ' | cut -d ' ' -f 5,9 | sed 's/on/▣/g' | sed 's/off/▢/g')
 }
 
+# Dropbox daemon status... kind of
 get_dropbox() {
     if [ -e "/usr/bin/dropbox" ]; then
         /usr/bin/dropbox running
         if [ "$?" -eq 1 ]; then
             _st=$(/usr/bin/dropbox status)
             if [ "${_st}" == "Up to date" ]; then
-                DROPBOX="U"
+                MON_DROPBOX="U"
             else
-                DROPBOX="W"
+                MON_DROPBOX="W"
             fi
         else
-            DROPBOX="X"
+            MON_DROPBOX="X"
         fi
     else
-        DROPBOX="-"
+        MON_DROPBOX="-"
     fi
 }
 
+# Processor or package temp. Heavily dependent on machine
 get_temp() {
-    TEMP=""
+    _temp=""
     for i in $(seq 0 1); do
-        TEMP=${TEMP}" $(($(cat /sys/class/thermal/thermal_zone$i/temp)/1000))℃"
+        _temp=${_temp}" $(($(cat /sys/class/thermal/thermal_zone$i/temp)/1000))℃"
     done
-    TEMP=${TEMP}
+    MON_TEMP=${_temp}
 }
 
+# Volume
 get_volu() {
-    VOL="$(amixer -c$1 sget Master | grep -Eo '[0-9]+%')"
+    MON_VOL="$(amixer -c$1 sget Master | grep -Eo '[0-9]+%')"
 }
 
+# Date and time
 get_date() {
-    DATE="$(date '+%d/%m %H:%M')"
+    MON_DATE="$(date '+%d/%m %H:%M')"
+    # With week number
+    #MON_DATE="$(date '+%d/%m %H:%M (%V)')"
 }
 
+# Battery level. Also dependent on machine
 get_batt() {
-    _low_limit=15
+    _low_limit=$1
 
     _cap="$(cat /sys/class/power_supply/BAT0/capacity)"
     _sts="$(cat /sys/class/power_supply/BAT0/status)"
@@ -57,32 +65,42 @@ get_batt() {
     else
         _st="?"
     fi
-    BAT="$_cap%$_st"
+    MON_BAT="$_cap%$_st"
 }
 
+# Load average. See
 get_load() {
-    LOADAVG="$(cut -d' ' -f1-3 /proc/loadavg)"
+    MON_LOADAVG="$(cut -d' ' -f1-3 /proc/loadavg)"
 }
 
+# Network up/downlink volume transferred
 get_netw() {
     if=$1
 
     _rxmib=$(bc <<< "scale=2; $(cat /sys/class/net/${if}/statistics/rx_bytes)/1024/1024")
     _txmib=$(bc <<< "scale=2; $(cat /sys/class/net/${if}/statistics/tx_bytes)/1024/1024")
 
-    NETW="$if: ${_rxmib}|${_txmib}⇵ MiB"
+    MON_NETW="$if: ${_rxmib}|${_txmib}⇵ MiB"
 }
+
+# Modify variables here
+# Network interface
+CFG_NETIF=wlo1
+# Audio card for volume monitoring
+CFG_AUD_C=1
+# Battery low level (%) for alert display
+CFG_BAT_LO=15
 
 while true; do
     get_temp
-    get_batt
-    get_volu 1
+    get_batt ${CFG_BAT_LO}
+    get_volu ${CFG_AUD_C}
     get_date
     get_load
-    get_netw wlo1
+    get_netw ${CFG_NETIF}
     get_dropbox
     get_mods
 
-    xsetroot -name "[$MODS] [$DROPBOX] [$NETW] [vol: $VOL] [load: $LOADAVG] [temp:$TEMP] [bat: $BAT] [$DATE]"
+    xsetroot -name "[$MON_MODS] [$MON_DROPBOX] [$MON_NETW] [vol: $MON_VOL] [load: $MON_LOADAVG] [temp:$MON_TEMP] [bat: $MON_BAT] [$MON_DATE]"
     sleep 5
 done
