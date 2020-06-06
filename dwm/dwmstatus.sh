c2a1105a9acf884ed30a062a9580cb8c6d7468ab
sebhz <seb@zyglute.fr>
2020-06-06 09:21:40 +0200

    [dwm] Updates to status script
        - Added dropbox monitor
        - Added keyboard modifier monitor
        - A couple of modifications and improvements

--- dwm/dwmstatus.sh
+++ dwm/dwmstatus.sh
@@ -1,29 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+
+get_mods() {
+    MODS="CN: "$(xset q | grep Caps | tr -s ' ' | cut -d ' ' -f 5,9 | sed 's/on/▣/g' | sed 's/off/▢/g')
+}
+
+get_dropbox() {
+    if [ -e "/usr/bin/dropbox" ]; then
+        /usr/bin/dropbox running
+        if [ "$?" -eq 1 ]; then
+            _st=$(/usr/bin/dropbox status)
+            if [ "${_st}" == "Up to date" ]; then
+                DROPBOX="U"
+            else
+                DROPBOX="W"
+            fi
+        else
+            DROPBOX="X"
+        fi
+    else
+        DROPBOX="-"
+    fi
+}
 
 get_temp() {
     TEMP=""
-    for i in $(seq 0 $(($(nproc)-1))); do
+    for i in $(seq 0 1); do
         TEMP=${TEMP}" $(($(cat /sys/class/thermal/thermal_zone$i/temp)/1000))℃"
     done
     TEMP=${TEMP}
 }
 
 get_volu() {
-    VOL="$(amixer -c0 sget Master | grep -Eo '[0-9]+%')"
+    VOL="$(amixer -c$1 sget Master | grep -Eo '[0-9]+%')"
 }
 
 get_date() {
-    DATE="$(date '+%d/%m %H:%M (WW%V)')"
+    DATE="$(date '+%d/%m %H:%M')"
 }
 
 get_batt() {
+    _low_limit=15
+
     _cap="$(cat /sys/class/power_supply/BAT0/capacity)"
     _sts="$(cat /sys/class/power_supply/BAT0/status)"
 
     if [ "$_sts" = "Charging" ]; then
         _st="+"
     elif [ "$_sts" = "Discharging" ]; then
-        _st="-"
+        if [ $_cap -lt $_low_limit ]; then
+            _st="!"
+        else
+            _st="-"
+        fi
     elif [ "$_sts" = "Full" ]; then
         _st="="
     else
@@ -48,11 +76,13 @@ get_netw() {
 while true; do
     get_temp
     get_batt
-    get_volu
+    get_volu 1
     get_date
     get_load
-    get_netw enp0s25
+    get_netw wlo1
+    get_dropbox
+    get_mods
 
-    xsetroot -name "[$NETW] [load: $LOADAVG] [temp:$TEMP] [bat: $BAT] [vol: $VOL] [$DATE]"
+    xsetroot -name "[$MODS] [$DROPBOX] [$NETW] [vol: $VOL] [load: $LOADAVG] [temp:$TEMP] [bat: $BAT] [$DATE]"
     sleep 5
 done
