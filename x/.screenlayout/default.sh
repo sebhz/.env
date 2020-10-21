df8572472312c06c977c3ec6e1f531353a7ee764
sebhz <seb@zyglute.fr>
2020-10-21 18:42:21 +0200

    [x] Added xrandr init scripts

---
+++ x/.screenlayout/default.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Is my external monitor connected ?
+xrandr | grep DP-1-1 | grep connected
+
+if [ $? = 0 ]; then
+    xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+fi
+
