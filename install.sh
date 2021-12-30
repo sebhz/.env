63909a5ba277647cd00f06d377930487e4d710ca
sebhz <seb@zyglute.fr>
2021-12-30 07:10:20 +0100

    [i3] Sane dir structure for stow

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # dwm is a bit special. Stowing it has no sense.
-stow -v emacs
 stow -v git
-# notion3 kept for reference - will be dropped.
-stow -v notion4
+stow -v i3
 stow -v pylint
 stow -v screen
 stow -v vim
-stow -v vnc
 stow -v x
 
